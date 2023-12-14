35028143db297a306de4ee752024d648ca527f09
sarthaksarthak9 <sarthaknegi908@gmail.com>
2023-12-14 20:22:58 +0530

    main file

---
+++ node/index.js
@@ -0,0 +1,5 @@
+//node.js automatically wrap any function and make it a wrapper function
+
+const lovish = require("./second");
+
+console.log("hello world", lovish) // (second.js playing like common js module)
